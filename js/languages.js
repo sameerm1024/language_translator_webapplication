ee77ae831ed5deec98effd7efc3601c8a69cf86f
Mohammad Sameer <2mohdsameer1024@gmail.com>
2024-07-24 13:52:45 +0530

    project commit

---
+++ js/languages.js
@@ -0,0 +1,9 @@
+const languages = {
+    "bn-IN": "Bengali",
+    "en-GB": "English",
+    "gu-IN": "Gujarati",
+    "hi-IN": "Hindi",
+    "pa-IN": "Punjabi",
+    "ta-LK": "Tamil",
+    "te-IN": "Telugu"
+}
